b35cc4fa5deea67ef59a5f1bcc5e09ea1fcea769
W9601166 <w9623647@live.tees.ac.uk>
2023-11-06 15:57:33 +0000

    Downgraded to support Flamingo version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ buildscript {
 
     dependencies {
         classpath("com.google.gms:google-services:4.4.0")
-        classpath("com.android.tools.build:gradle:8.1.2")
+        classpath("com.android.tools.build:gradle:8.0.2")
     }
     repositories {
         google()
@@ -11,7 +11,7 @@ buildscript {
     }
 }
 plugins {
-    id("com.android.application") version "8.1.2" apply false
+    id("com.android.application") version "8.0.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
 }
